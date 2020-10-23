9dcbac23df540dd99f5129e4eef65ab4912eec4d
shabeerali-kh <shabeeralikh@gmail.com>
2020-10-23 10:40:40 +0530

    connection handler upd

--- truYum/src/com/cognizant/truyum/model/Cart.java
+++ truYum/src/com/cognizant/truyum/model/Cart.java
@@ -22,6 +22,39 @@ public class Cart {
 		this.menuItemList = menuItemList;
 		this.total = total;
 	}
+	@Override
+	public String toString() {
+		return "Cart [menuItemList=" + menuItemList + ", total=" + total + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((menuItemList == null) ? 0 : menuItemList.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(total);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cart other = (Cart) obj;
+		if (menuItemList == null) {
+			if (other.menuItemList != null)
+				return false;
+		} else if (!menuItemList.equals(other.menuItemList))
+			return false;
+		if (Double.doubleToLongBits(total) != Double.doubleToLongBits(other.total))
+			return false;
+		return true;
+	}
+	
 	
 	
 }
